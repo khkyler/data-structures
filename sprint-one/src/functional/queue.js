d9cf9492e104a03aa416fe32a3f9e47c06f880e4
khkyler <karim.kyler@gmail.com>
2014-12-10 15:09:35 -0800

    solved the functional approach for que and stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,31 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+
+    for (var key in storage) {
+      var newKey = parseInt(key) + 1;
+      storage[newKey] = storage[key];
+    };
+    counter++;
+    storage[0] = value;
   };
 
   someInstance.dequeue = function(){
+    if (counter > 0) {
+      var value = storage[counter - 1];
+      delete storage[counter - 1];
+      counter--;
+      return value;
+    }
   };
 
   someInstance.size = function(){
+    return counter;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,25 @@ var Stack = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var counter = 0;
+
   // Implement the methods below
   someInstance.push = function(value){
+    counter++;
+    storage[counter] = value;
   };
 
   someInstance.pop = function(){
+    if (counter > 0) {
+      var value = storage[counter];
+      delete storage[counter];
+      counter--;
+      return value;
+    }
   };
 
   someInstance.size = function(){
+    return counter;
   };
 
   return someInstance;

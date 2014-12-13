0eb3ba1bbab7669827c121036f7d37b21a11043b
khkyler <karim.kyler@gmail.com>
2014-12-12 16:40:36 -0800

    Working on HashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,21 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var tuple = [k,v];
+  this._storage[i] = tuple;
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  return this._storage[i][1];
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  //debugger;
 
+  this._storage[i].splice(i,1,null);
+  console.log(this._storage[i]);
 };
 
 
